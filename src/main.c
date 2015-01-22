9893d9d4eb5e222ee57d0f639c3d1942e21e1093
Christopher Kruczek <christopher.kruczek@gmail.com>
2015-01-22 14:49:04 +0100

    Added some testing code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** texpack related includes **/
+#include "cmdline.h"
 
 int main(int argc, char* argv[])
 {
-
+    OptionType t = SYNC;
+    printf("%d\n",argc);
+    printf("%s\n",argv[0]);
+    printf("%d\n",t);
 }
